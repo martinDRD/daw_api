23ed8f870286b8960fed049ad72b7f004db38e56
martinDRD <mario.malopez@alumnos.upm.es>
2018-05-11 09:06:31 +0200

    Initil Commit

---
+++ daw_api/catalog/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Movie
+
+# Register your models here.
+#admin.site.register(Book)
+#admin.site.register(Author)
+#admin.site.register(Genre)
+admin.site.register(Movie)
+#admin.site.register(BookInstance)
+
+"""# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+# Register the Admin classes for Book using the decorator
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    def display_genre(self):
+        """
+"""
+    Creates a string for the Genre. This is required to display genre in Admin.
+"""
+"""
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+"""
